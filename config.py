423167e73349ee97bfa1280b8c55eb5afb6a157f
didierorduzz <andreyyorduz@gmail.com>
2025-05-03 20:35:51 -0500

    Mejoras y arreglos generales en sql y vista de administrador (dashboard)

--- config.py
+++
@@ -1,3 +0,0 @@
-DB_USERNAME = "didierorduzz"
-DB_PASSWORD = "Didierorduz1"
-DB_DSN = "localhost/XE"  # XE es el SID por defecto de Oracle XE
---
+++ sql/insertar_usuarios.sql
@@ -0,0 +1,5 @@
+INSERT INTO usuarios (nombre, correo, contrasena, rol)
+VALUES ('Admin supremo', 'admin@example.com', '1234', 'administrador');
+
+INSERT INTO usuarios (nombre, correo, contrasena, rol)
+VALUES ('Profe Didier', 'dorduz3@udi.edu.co', 'abcd', 'profesor');
