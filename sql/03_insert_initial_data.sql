d9ec2d68c52455cbeb296ab039634a01dc25729d
didierorduzz <andreyyorduz@gmail.com>
2025-05-09 22:50:07 -0500

    Eliminados los scripts del seguimiento de git

--- sql/03_insert_initial_data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO usuarios (nombre, correo, contrasena, rol)
-VALUES ('Administrador', 'admin@udi.edu.co', 'admin123', 'administrador');
-
-INSERT INTO usuarios (nombre, correo, contrasena, rol)
-VALUES ('Profesor Juan', 'juan@udi.edu.co', '1234', 'profesor');
